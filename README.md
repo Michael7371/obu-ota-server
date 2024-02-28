94a965b9eb19eeeb3fb202e96644ab145984b2d9
Michael7371 <michael@neaera.net>
2024-02-28 08:38:29 -0700

    updates after testing with commsignia obu

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # obu-ota-server
 Nginx server to host firmwares for On Board Devices (OBU's)
+
+## Setup
+Make a copy of the `sample.env` file and rename it to `.env` with all relevenat variables filled out. After the .env is configured run `docker compose up` to spin up the nginx proxy and the fast api backend. 
--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 from typing import Union
-from fastapi import FastAPI, Request
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Request, Response, HTTPException, status
+from fastapi.responses import StreamingResponse, FileResponse
 from app import manifest
 import os
 import glob
-from fastapi import HTTPException
+import aiofiles
+from starlette.responses import Response
+import logging
 
 app = FastAPI()
+log_level = (
+        "INFO" if "LOGGING_LEVEL" not in os.environ else os.environ["LOGGING_LEVEL"]
+    )
 
+logging.basicConfig(format="%(levelname)s:%(message)s", level=log_level)
+
+async def file_iterator(file_path: str):
+    with open(file_path, "rb") as file:
+        while chunk := file.read(8192):
+            yield chunk
 
 @app.get("/")
 def read_root(request: Request):
@@ -17,15 +28,45 @@ def read_root(request: Request):
 def get_manifest(request: Request):
     try:
         files = glob.glob("/firmwares/*.tar.sig")
-        print(files)
+        logging.debug(files)
         host_name = os.getenv("SERVER_HOST","localhost")
         response_manifest = manifest.add_contents(host_name, files)
-        print(response_manifest)
+        logging.debug(response_manifest)
         return response_manifest
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@app.get("/firmwares/{firmware_id}")
-def get_fw(firmware_id: str):
-    return FileResponse(f"/firmwares/{firmware_id}")
 
+@app.get("/firmwares/{firmware_id}")
+async def get_fw(request: Request, firmware_id: str):
+    logging.debug(f"Method: {request.method}")
+    logging.debug(f"URL: {request.url}")
+    logging.debug(f"Headers: {request.headers}")
+    logging.debug(f"Query Parameters: {request.query_params}")
+    logging.debug(f"Client: {request.client}")
+    logging.debug(f"Cookies: {request.cookies}")
+    file_path = f"/firmwares/{firmware_id}"
+    range_header = request.headers.get('Range')
+    start, end = 0, None
+
+    if range_header:
+        start_end = range_header.partition('=')[2].split('-')
+        start = int(start_end[0])
+        end = int(start_end[1]) if start_end[1] else None
+
+    async with aiofiles.open(file_path, mode='rb') as file:
+        file_size = os.path.getsize(file_path)
+        if end is None:
+            end = file_size
+        await file.seek(start)
+        data = await file.read(end - start)
+
+    headers = {
+        'Content-Range': f'bytes {start}-{end-1}/{file_size}',
+        'Content-Length': str(end - start),
+        'Accept-Ranges': 'bytes',
+    }
+
+    logging.debug(f"Completed Firmware Response for lient: {request.client}")
+    return Response(content=data, media_type='application/octet-stream', headers=headers)
+    
